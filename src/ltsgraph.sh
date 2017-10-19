c9a4996204125425f5685fa4b1711842672cd243
sjef <sjefvanloo@gmail.com>
2017-10-19 17:39:25 +0200

    Updated modul formulas and model

---
+++ src/ltsgraph.sh
@@ -0,0 +1,1 @@
+mcrl22lps -lregular2 alles.mcrl2 alles.lps && lps2lts -rjittyc alles.lps alles.lts && ltsgraph alles.lts
---
+++ src/verify.sh
@@ -0,0 +1,1 @@
+mcrl22lps -lregular2 alles.mcrl2 alles.lps && lps2pbes alles.lps t.pbes -freq2a.mcf && pbes2bool t.pbes --rewriter=jittyc -v
