3f5a731f14890795c27baf5b112e38134857080f
sjef <sjefvanloo@gmail.com>
2017-10-21 15:57:04 +0200

    Implemented some stuff

--- src/ltsgraph.sh
+++ src/ltsgraph.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-mcrl22lps -lregular marc.mcrl2 alles.lps \
+mcrl22lps -lregular2 marc.mcrl2 alles.lps \
     && lps2lts -rjittyc alles.lps alles.lts \
     && ltsgraph alles.lts
--- src/verify_all.sh
+++ src/verify_all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 for file in *.mcf; do
     echo "${file}"
-    mcrl22lps -lregular2 alles.mcrl2 alles.lps \
+    mcrl22lps -lregular2 marc.mcrl2 alles.lps \
         && lps2pbes alles.lps t.pbes "-f${file}" \
         && pbes2bool t.pbes --rewriter=jittyc
 done
