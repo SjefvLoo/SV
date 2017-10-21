44e3210ff1835dd4afbf2678da148c1c2a11638f
sjef <sjefvanloo@gmail.com>
2017-10-21 16:20:57 +0200

    Zaken

--- src/verify.sh
+++ src/verify.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-mcrl22lps -lregular2 alles.mcrl2 alles.lps \
+mcrl22lps -lregular2 marc.mcrl2 alles.lps \
     && lps2pbes alles.lps t.pbes -freq2a.mcf \
     && pbes2bool t.pbes --rewriter=jittyc -v
