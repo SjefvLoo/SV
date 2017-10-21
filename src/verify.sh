6e267348a0e03f352d4d0654f45399d804a269d3
sjef <sjefvanloo@gmail.com>
2017-10-21 17:33:58 +0200

    Updated req2b

--- src/verify.sh
+++ src/verify.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 mcrl22lps -lregular2 marc.mcrl2 alles.lps \
-    && lps2pbes alles.lps t.pbes -freq2a.mcf \
+    && lps2pbes alles.lps t.pbes -freq$1.mcf \
     && pbes2bool t.pbes --rewriter=jittyc -v
