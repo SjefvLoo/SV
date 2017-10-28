eedfe325f385858e6af0b7da2b1e818938929a6a
Marc <>
2017-10-28 13:52:19 +0200

    add results to verify script as well

--- src/verify.sh
+++ src/verify.sh
@@ -12,10 +12,11 @@ if [ -z "$1" ]; then
     for file in requirements/*.mcf; do
         before="$(date +%s)"
         echo "${file}"
-        lps2pbes "${MCRL2_NAME}.lps" "${file%.mcf}.pbes" "-f${file}" \
-		&& pbes2bool "${file%.mcf}.pbes" --rewriter=jittyc
+        result="$(lps2pbes "${MCRL2_NAME}.lps" "${file%.mcf}.pbes" "-f${file}" \
+		&& pbes2bool "${file%.mcf}.pbes" --rewriter=jittyc)"
+        result="$( [ "${result}" = 'true' ] && echo 'true' || echo 'false')"
         after="$(date +%s)"
-        echo "${file},${before},${after}"
+        echo "${file},${result},${before},${after}"
     done
 else
     mcrl22lps -lregular2 "${MCRL2_NAME}.mcrl2" "${MCRL2_NAME}.lps" \
