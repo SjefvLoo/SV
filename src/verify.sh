479f416c507ed2b1cc10673482acb787f57764c8
Marc <>
2017-10-28 13:07:12 +0200

    add performance to verify.sh

--- src/verify.sh
+++ src/verify.sh
@@ -10,9 +10,12 @@ fi
 if [ -z "$1" ]; then
     mcrl22lps -lregular2 "${MCRL2_NAME}.mcrl2" "${MCRL2_NAME}.lps" || exit 1
     for file in requirements/*.mcf; do
+        before="$(date +%s)"
         echo "${file}"
         lps2pbes "${MCRL2_NAME}.lps" "${file%.mcf}.pbes" "-f${file}" \
 		&& pbes2bool "${file%.mcf}.pbes" --rewriter=jittyc
+        after="$(date +%s)"
+        echo "${file},${before},${after}"
     done
 else
     mcrl22lps -lregular2 "${MCRL2_NAME}.mcrl2" "${MCRL2_NAME}.lps" \
